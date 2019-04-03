6327e87ca3d8e4808d97eb4835a3630dcf109c26
495036225@qq.com <495036225@qq.com>
2019-04-03 21:57:31 +0800

    qq

---
+++ TipCalculator/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TipCalculator
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    /// 
+    
+    public partial class MainWindow : Window
+    {
+        Tip tipcal=new Tip();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        //选定一个单选按钮时，
+        //进行计算
+        private void RadioButtonClick(object sender, RoutedEventArgs e)
+        {
+            performCal();
+        }
+        //执行计算
+        private void performCal()
+        {
+            var selectRadio= this.MyStackPanel.Children.OfType<RadioButton>().FirstOrDefault(r => r.IsChecked == true);
+            try {
+                tipcal.calTip(this.tipcal.billAmount, double.Parse(selectRadio.Tag.ToString()));
+            }catch(Exception)
+            {
+                MessageBox.Show("请选择一个单选框");
+                return;
+            }
+            this.TipAmount.Content = "TipAmount: ￥"+tipcal.tipAmount.ToString("f2");
+            this.TotalAmount.Content = "TotalAmount: ￥" + tipcal.totalAmount.ToString("f2");
+        }
+
+        private void lostFocus(object sender, RoutedEventArgs e)
+        {
+            performCal();
+        }
+        //按下回车键后，将TextBox中的数字转换成货币表示
+        private void keyDown(object sender, KeyEventArgs e)
+        {
+            double billam = 0.00;
+            try
+            {
+                billam = double.Parse(this.BillAmount.Text.ToString());
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Bill 格式错误");
+            }
+            this.tipcal.billAmount = billam;
+            this.BillAmount.Text = "￥" + String.Format("{0:F}", billam);
+        }
+
+        private void TextChange(object sender, TextChangedEventArgs e)
+        {
+        }
+    }
+}
---
+++ TipCalculator/Tip.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipCalculator
+{
+    class Tip
+    {
+        public double tipAmount { get; set; }
+        public double billAmount { get; set; }
+        public double totalAmount { get; set; }
+        public Tip()
+        {
+            this.billAmount = 0.00;
+            this.tipAmount = 0.00;
+            this.totalAmount = 0.00;
+        }
+        public void calTip(double billAmount, double percent)
+        {
+            this.billAmount = billAmount;
+            this.tipAmount = billAmount * percent;
+            this.totalAmount = this.billAmount + this.tipAmount;
+        }
+    }
+}
